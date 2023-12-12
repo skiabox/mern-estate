dc352783c75793d526fea0dee41a6a16162685be
Stavros Kefaleas <administrator@Belkin.fritz.box>
2023-12-12 16:01:51 +0200

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+//next is used when we want to use a middleware
 export const signup = async (req, res, next) => {
   // console.log("REQ BODY ON SIGNUP", req.body);
   // get the request body parameters
@@ -15,6 +17,30 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     // res.status(500).json(error.message);
     //pass the error to the error handler middleware
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    console.log(validUser);
+    console.log(validUser._doc);
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    //if user emaul exists compare the password with tha hashed password of the database user object
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    //destructuring the user object to remove the password field
+    const { password: pass, ...rest } = validUser;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
